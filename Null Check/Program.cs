04b098cc68d2f7d0df9747f2e0db14256f2cfd20
hp <hp@DESKTOP-NC0MAAO>
2022-03-26 07:02:25 +0500

    Add project files.

---
+++ Null Check/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Method1(object obj)
+{
+    if (obj is null)
+    {
+        throw new ArgumentNullException(nameof(obj));
+    }
+}
+
+void Method2(object param)
+{
+    ArgumentNullException.ThrowIfNull(param);
+}
